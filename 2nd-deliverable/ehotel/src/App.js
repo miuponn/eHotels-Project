e32d9b58a94faf2cdc465d021d437f8924408cf1
Kelly Gao <kellygao@live.ca>
2024-03-21 20:39:01 -0400

    removed client

--- 2nd-deliverable/ehotel/src/App.js
+++ 2nd-deliverable/ehotel/src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hiii!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'App'>
+      <h1>Make yourself at home.</h1>
     </div>
   );
 }
-
-
 
 export default App;
