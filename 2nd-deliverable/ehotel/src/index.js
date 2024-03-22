e241432b3f930b7d1f917c34f6570bf72c984423
Anthony Le <theant741@gmail.com>
2024-03-21 20:00:02 -0400

    added ActionAreaCard for test

--- 2nd-deliverable/ehotel/src/index.js
+++ 2nd-deliverable/ehotel/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './components/Card.js';
+import ActionAreaCard from './components/Card.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ActionAreaCard />
   </React.StrictMode>
 );
 
