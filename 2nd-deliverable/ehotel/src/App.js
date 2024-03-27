4508cfbbabe7477e1d4f9300369bd6ed9ef05e5d
Anthony Le <theant741@gmail.com>
2024-03-27 16:00:11 -0400

    added some testing code to the App.js and its css file

--- 2nd-deliverable/ehotel/src/App.js
+++ 2nd-deliverable/ehotel/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import {Routes, Route} from "react-router-dom";
+import Hotels from "./pages/Hotels.js";
+
 
 function App(){
   return (
     <div className = 'App'>
+      <Navbar />
+      <Routes>
+        <Route path="/home" />
+        <Route path="/hotels" element= {<Hotels />} />
+      </Routes>
       <h1>Make yourself at home.</h1>
     </div>
   );
