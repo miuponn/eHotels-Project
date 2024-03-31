2b072a5e153cef35b1af5f08739de0827ebb5f1c
Anthony Le <theant741@gmail.com>
2024-03-30 23:53:15 -0400

    changed Hotel code for the array and the hotels for testing

--- 2nd-deliverable/ehotel/src/pages/Hotels.js
+++ 2nd-deliverable/ehotel/src/pages/Hotels.js
@@ -2,6 +2,16 @@ import React from "react";
 import Card from "../components/Card";
 import "./Hotels.css"
 
+import Image1 from "./OIP.jpg";
+import Image2 from "./temphotel.jpeg";
+const hotels = [
+    {id: 1, name: "Mariott",imageUrl: Image1},
+    {id: 2, name: "Hotel Escapade", imageUrl: Image2},
+    {id: 3, name: "Nice Hotel",imageUrl: Image1},
+    {id: 4, name: "sexy Hotel", imageUrl: Image2},
+    {id: 5, name: "beautiful Hotel",imageUrl: Image1},
+    {id: 6, name: "Hi Hotel", imageUrl: Image2},
+];
 
 export const Hotels = () => {
     return (
@@ -9,12 +19,13 @@ export const Hotels = () => {
         <p className="hotels-description">
             Hi! there is nothing on this page right now. It will show the hotels available within this place.
             </p>
-        <br></br>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+        <br/>
+        {hotels.map(hotel => (
+           <Card 
+            hotelName={hotel.name}
+            imageSrc={hotel.imageUrl}
+            />
+        ))}
     </div> 
     );
 };
