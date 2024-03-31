aa65f9e273196f7e77b5a354fd8bd89d4f944ade
Anthony Le <theant741@gmail.com>
2024-03-30 23:49:59 -0400

    added Card and css mods to account for parameters

--- 2nd-deliverable/ehotel/src/components/Card.js
+++ 2nd-deliverable/ehotel/src/components/Card.js
@@ -7,21 +7,30 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';*/
-
-import tempHotelPic from './temphotel.jpeg';
 import "./Card.css";
 
 
-function Card() {
+function Card({ hotelName, imageSrc }) {
     return (
-        <a href="/Hotel" className="card-link">
+        <a href="/hotel" className="card-link">
         <div className="card">
-            <img className="card-image"src={tempHotelPic} alt="temporary hotel"></img>
-            <h2 className="card-header">Hotel</h2>
-            <p className="card-text">Hotel. Very good. Very Sexy. I like.</p>
+            <img className="card-image"src={imageSrc} alt={hotelName}></img>
+            <h2 className="card-header">{hotelName}</h2>
+            <p className="card-text">Hotel. Very good. Very sexy. I like. </p>
         </div>
         </a>
     );
 };
 
+
+/* <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+
+            </ul>
+    will add the features later with included icons.        
+            */
+// <li> will have the features available for each hotel. but if not; then we'll have nothing (especially if it's a hotel chain)
+// later on; will replace the description with the {description} parameter. 
 export default Card;
