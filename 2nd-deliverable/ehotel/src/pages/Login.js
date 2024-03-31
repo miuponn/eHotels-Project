22f0494c883c0b5d5b42aceb949e3f66c23118ae
Anthony Le <theant741@gmail.com>
2024-03-30 23:53:55 -0400

    added login page modifications; done and ready to go

--- 2nd-deliverable/ehotel/src/pages/Login.js
+++ 2nd-deliverable/ehotel/src/pages/Login.js
@@ -1,23 +1,35 @@
 import React from "react";
-
-
+import "./Login.css";
 
 function Login() {
     return(
-        <div className="login-container">
-            <form classNae="login-form">        
-                <div className="user-info">
-                    <label for="username"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="password" required/>
-                    <label for="password"><b>Password</b></label>
-                    <input type="text" placeholder="Enter Password" name="password" required/>
-                    <button type="submit">Login</button>
+            <div className="login-container">    
+                <div className="login-box">    
+                        <div className="login-header">
+                            <header>Login</header>
+                        </div>
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="Email" autocomplete="off" required></input>
+
+                        </div>
+                        <div className="input-box">
+                            <input type="password" className="input-field" placeholder="Password" autocomplete="off" required></input>
+                        </div>
+                        <div className="forgot">
+                            <section>
+                                <input type="checkbox" id="check"></input>
+                                <label for="check">Remember me</label>
+                            </section>
+                        </div>
+                        <div className="input-submit">
+                            <button className="submit-btn" id="submit"></button>
+                            <label for="submit">Login</label>
+                        </div>
+                        <div className="sign-up-link">
+                            <p>Don't have an account? <a href="/signUp">Sign up here</a></p>
+                        </div>
+                    </div>
                 </div>
-            </form>
-        <div>   
-            <p>Don't have an account? <a href="/signup">Sign up here.</a></p>
-            </div>
-        </div>
     );
 }
 
