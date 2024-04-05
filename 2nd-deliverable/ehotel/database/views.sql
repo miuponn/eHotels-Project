6121261b76b1f91ba1881c902e740e218ad30f6a
Kelly Gao <kellygao@live.ca>
2024-04-05 17:07:08 -0400

    Update views.sql + added sql files

--- 2nd-deliverable/ehotel/database/views.sql
+++ 2nd-deliverable/ehotel/database/views.sql
@@ -26,3 +26,63 @@ JOIN
 	HOTEL h ON r.HotelID = h.HotelID
 GROUP BY 
 	h.HotelID
+
+-- View by hotel chain & cateogory (rating)
+CREATE VIEW HotelsByChainAndCategory AS
+SELECT
+	hc.HotelChainID,
+	h.HotelID,
+	h.Category
+FROM
+	HOTEL h
+JOIN
+	HOTEL_CHAIN hc ON h.HotelChainID = hc.HotelChainID
+ORDER BY 
+	hc.HotelChainID, h.Category;
+
+-- View of hotel room pricing 
+CREATE VIEW HotelRoomPrices AS
+SELECT
+    h.HotelID,
+    MIN(r.Price) AS MinRoomPrice,
+    AVG(r.Price) AS AvgRoomPrice,
+    MAX(r.Price) AS MaxRoomPrice
+FROM 
+    ROOM r
+JOIN 
+    HOTEL h ON r.HotelID = h.HotelID
+GROUP BY
+    h.HotelID;
+
+CREATE VIEW AvailableRoomCapacitiesByHotel AS
+SELECT
+    h.HotelID,
+    h.Name AS HotelName,
+    SUM(CASE WHEN r.Capacity = 1 AND (res.ReservationID IS NULL OR res.Status = 'Cancelled') THEN 1 ELSE 0 END) AS SingleRooms,
+    SUM(CASE WHEN r.Capacity = 2 AND (res.ReservationID IS NULL OR res.Status = 'Cancelled') THEN 1 ELSE 0 END) AS DoubleRooms,
+    SUM(CASE WHEN r.Capacity = 3 AND (res.ReservationID IS NULL OR res.Status = 'Cancelled') THEN 1 ELSE 0 END) AS TripleRooms,
+    SUM(CASE WHEN r.Capacity = 4 AND (res.ReservationID IS NULL OR res.Status = 'Cancelled') THEN 1 ELSE 0 END) AS QuadRooms
+FROM 
+    ROOM r
+JOIN 
+    HOTEL h ON r.HotelID = h.HotelID
+LEFT JOIN 
+    RESERVATION res ON r.HotelID = res.HotelID AND r.RoomNumber = res.RoomNumber
+GROUP BY
+    h.HotelID, h.Name;
+
+-- View amenities available in each hotel
+CREATE VIEW HotelsByAvailableAmenities AS
+SELECT
+    h.HotelID,
+    MAX(CASE WHEN r.Amenities_AC = TRUE AND (res.ReservationID IS NULL OR res.Status = 'Cancelled') THEN 1 ELSE 0 END) AS Has_AC,
+    MAX(CASE WHEN r.Amenities_TV = TRUE AND (res.ReservationID IS NULL OR res.Status = 'Cancelled') THEN 1 ELSE 0 END) AS Has_TV,
+    MAX(CASE WHEN r.Amenities_Jacuzzi = TRUE AND (res.ReservationID IS NULL OR res.Status = 'Cancelled') THEN 1 ELSE 0 END) AS Has_Jacuzzi
+FROM 
+    ROOM r
+JOIN 
+    HOTEL h ON r.HotelID = h.HotelID
+LEFT JOIN 
+    RESERVATION res ON r.HotelID = res.HotelID AND r.RoomNumber = res.RoomNumber
+GROUP BY
+    h.HotelID;
